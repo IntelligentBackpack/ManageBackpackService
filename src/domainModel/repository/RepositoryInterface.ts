beff8af21176131c00ff2db91b735cf7c4d1f427
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-19 19:56:18 +0200

    chore: added a function field for register a device

--- src/domainModel/repository/RepositoryInterface.ts
+++ src/domainModel/repository/RepositoryInterface.ts
@@ -6,6 +6,9 @@ export interface Repository {
 
     addDevice: (hash: string, deviceId: string) => Promise<deviceModule.RegisteredDeviceImpl>;
 
-    registerDevice: (hash: string, email: string) => Promise<string>;
+    registerDevice: (hash: string, email: string, deviceId: string) => Promise<string>;
+
+    //TODO tolgo la mail nel record all'interno del db, cancello la relazione su firebase e mando un messaggio DISS al device
+    //disassociateBackpack: (hash: string) => void;
 
 }
