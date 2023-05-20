07e4dc930ef95ce7223c47c9c65215969a6e7aa0
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2023-05-20 18:46:15 +0200

    build: added release configuration

---
+++ release.config.js
@@ -0,0 +1,18 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
+git push --force origin \${nextRelease.version} || exit 2
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            { "path": "build/libs/*.jar" },
+        ]
+    }],
+    "@semantic-release/git",
+)
+config.branches = [ "main" ]
+module.exports = config
